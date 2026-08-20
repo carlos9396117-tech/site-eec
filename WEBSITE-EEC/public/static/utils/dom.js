2a3332a13834cbd8e25dfb4c69b0cc6b7bc06a42
carlos9396117-tech <carlos.9396117@aluno.mg.gov.br>
2026-08-20 17:37:12 +0000

    inicio da criação do arquivo dom.js

---
+++ WEBSITE-EEC/public/static/utils/dom.js
@@ -0,0 +1,17 @@
+            parent.appendchild(
+(function () {
+    function normalizeText(text) {
+        return text === undeined || text === null ? '' : String(text);
+    }
+
+    function createElementSafe(tag, text = '', className = '') {
+        const element = document.createElement(tag);
+
+        if (className) {
+            element.className = className;
+        }
+
+        if (text !== undefined && text !)
+    }
+})
+            )
